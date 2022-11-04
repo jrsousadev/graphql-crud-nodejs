fa443140dbd9315078d65cedba020cee8376170d
Junior Sousa <jrsousadev@gmail.com>
2022-11-04 09:04:04 -0300

    fix internal error

---
+++ src/dtos/models/user-model.ts
@@ -0,0 +1,34 @@
+import { Field, ObjectType } from "type-graphql";
+
+@ObjectType()
+export class User {
+  @Field()
+  id: string;
+
+  @Field()
+  name: string;
+
+  @Field()
+  email: string;
+
+  @Field()
+  password: string;
+}
+
+@ObjectType()
+export class UserAuthenticated {
+  @Field()
+  id: string;
+
+  @Field()
+  name: string;
+
+  @Field()
+  email: string;
+
+  @Field()
+  password: string;
+
+  @Field()
+  token: string;
+}
